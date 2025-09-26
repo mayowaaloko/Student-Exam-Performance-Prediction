4ea2b78efbb65aa58f9150b58d8ec4453304b5be
daniel <mayowaaloko@gmail.com>
2025-09-26 08:54:25 +0100

    Model Trainer Done

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 
 def save_object(file_path, obj):
@@ -14,6 +15,41 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)  # Trains the model
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_mae = mean_absolute_error(y_train, y_train_pred)
+            test_mae = mean_absolute_error(y_test, y_test_pred)
+
+            train_mse = mean_squared_error(y_train, y_train_pred)
+            test_mse = mean_squared_error(y_test, y_test_pred)
+
+            train_r2 = r2_score(y_train, y_train_pred)
+            test_r2 = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = {
+                "train_mae": train_mae,
+                "test_mae": test_mae,
+                "train_mse": train_mse,
+                "test_mse": test_mse,
+                "train_r2": train_r2,
+                "test_r2": test_r2,
+            }
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
